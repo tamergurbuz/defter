c187e7b8cd4e271d49d9afcbdd9a59384c4f169d
Tamer Gürbüz <tamergurbuz@Tamer-MacBook-Air.local>
2026-05-30 12:03:41 +0300

    def ilk versiyon

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'defter-v3';
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(['./index.html']))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  // Her zaman önce network, cache fallback
+  e.respondWith(
+    fetch(e.request).then(res => {
+      if (res.ok) {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+      }
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
